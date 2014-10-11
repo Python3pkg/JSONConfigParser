108768166c660b0ba38da07b21f687d5813734f2
justanr <alecreiter@gmail.com>
2014-10-11 15:36:44 -0400

    Make sure correct functions are imported

--- jsonconfigparser/__init__.py
+++ jsonconfigparser/__init__.py
@@ -1,5 +1,5 @@
 from .configparser import JSONConfigParser
-from .utils import convert_input, dict_, list_, fieldtypes
-from .commands import view, add_file, add_field, delete, edit
+from .utils import dict_, list_, fieldtypes, command, call
+from .commands import add_file, add_field, view, edit, delete, append
 
 version = '0.0.1'
