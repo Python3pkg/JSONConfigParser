6a2b00377e84d88888466a25ce4156e41b858581
justanr <alecreiter@gmail.com>
2014-10-12 19:35:23 -0400

    Integrated build_converter functionality to the commands.
    Added build_converter functionality to add_field, edit and append.

--- jsonconfigparser/commands.py
+++ jsonconfigparser/commands.py
@@ -4,30 +4,62 @@ from operator import delitem
 
 from jsonpath_rw import parse
 
-from . import list_, dict_, fieldtypes
-from .utils import command, act_on_path, root, set_on_path
+from .utils import build_converter, command, act_on_path, root, set_on_path, root
 
 @command
 def view(json, path):
     '''A stored command for the view method of the JSONConfigParser
-    object.
+    object which pretty prints the endpoint's value.
+
+    :param json JSONConfigParser: The JSON representation to act on
+    :param path str: A string representation of a JSONpath endpoint
+    :returns None:
     '''
     json.view(path)
 
 @command
 def add_file(json, other):
     '''Updates the JSONConfigParser object with another JSON file.
+
+    :param json JSONConfigParser: The JSON representation to act on
+    :param other str: A filepath to the JSON file to concatenate.
+    :returns None:
     '''
     json.read(other)
 
 @command
-def add_field(json, path, value):
+def add_field(json, path, value, convert=False):
     '''Adds another field to the JSONConfigParser object.
+
+    :param json JSONConfigParser: The JSON representation to act on.
+    :param path str: A string representation of a JSONpath endpoint,
+        the end point should be the new field to add
+    :param value: The value to place at the new endpoint
+    :param convert str: A string representing the steps to convert the value to
+        it's final form.
+    :returns None:
     '''
+    if convert:
+        converter = build_converter(convert)
+        value = converter(value)
     set_on_path(json, path, value)
 
 @command
 def append(json, path, value, multi=False, convert=False):
+    '''Adds a value to a JSON collection at the endpoint.
+
+    :param json JSONConfigParser: The JSON representation to act on.
+    :param path str: A string representation of a JSONpath endpoint
+        If the endpoint is not an array/list or object/dict, this function will
+        raise a TypeError
+    :param value: The place to place into the specified endpoint
+    :param multi bool: If true, add the value to every matching endpoint.
+        The default is False and if multiple endpoints are present this function
+        will raise an AttributeError
+    :param convert str: A string representing the steps to convert the value to
+        it's final form.
+    :returns None:
+    '''
     expr = parse(path)
     matches = expr.find(json)
 
@@ -51,8 +83,9 @@ def append(json, path, value, multi=False, convert=False):
             return lambda j, f, v: j[f].update(v)
         return lambda j, f, v: j[f].append(v)
 
-    if convert in fieldtypes:
-        value = fieldtypes[convert](value)
+    if convert:
+        converter = build_converter(convert)
+        value = converter(value)
 
     for match in matches:
         action = guess_action(match.value)
@@ -60,16 +93,29 @@ def append(json, path, value, multi=False, convert=False):
         act_on_path(json, str(match.full_path), action)
 
 @command
-def delete(json, path=None):
+def delete(json, path=False):
     '''Deletes a JSONPath endpoint from the JSONConfigParser object.
+
+    :param json JSONConfigParser: The JSON representation to act on.
+    :param path str: A string representation of a JSONpath endpoint. If False,
+        it sets the JSON representation to a blank dictionary.
     '''
-    if not path or path == '$':
+    if not path or path == root:
         json = {}
     else:
         act_on_path(json, path, delitem)
 
 @command
-def edit(json, path, value):
+def edit(json, path, value, convert=False):
     '''Updates the value at the JSONPath endpoint.
+
+    :param json JSONConfigParser: The JSON representation to act on.
+    :param path str: A string representation of a JSONpath endpoint
+    :param value: The value to be placed at the endpoint
+    :param convert str: A string representing the steps to convert the value to
+        it's final form.
     '''
+    if convert:
+        converter = build_converter(convert)
+        value = converter(value)
     set_on_path(json, path, value)
